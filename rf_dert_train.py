0c1580ec1e6fda9ce558f08d7cebb8509e0c5f47
KrishBakshi <krishbakshi23@gmail.com>
2026-05-14 18:36:09 +0530

    Update: batch_size for x1 a100 80 gb

--- rf_dert_train.py
+++ rf_dert_train.py
@@ -1,12 +1,16 @@
+import warnings
+warnings.filterwarnings("ignore")
 from rfdetr import RFDETRSeg2XLarge
+
+#The follwing is for x1 a100 80GB SMX/SMX4 GPU
 if __name__ == "__main__":
     model = RFDETRSeg2XLarge()
 
     model.train(
         dataset_dir="./dataset/",
         epochs=100,
-        batch_size=4,
-        grad_accum_steps=4,
+        batch_size=8,
+        grad_accum_steps=2,
         lr=1e-4,
         output_dir="./output/",
         device="cuda",
